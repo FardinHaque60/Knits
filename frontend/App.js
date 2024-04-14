28c727b39010f812b5ff8f15f6508d7280490520
Fardin Haque <fardinhaque60@gmail.com>
2024-04-13 17:25:29 -0700

    set up structure for app

--- frontend/App.js
+++ frontend/App.js
@@ -1,31 +1,20 @@
 import React, { useEffect, useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
-import axios from 'axios';
+import { StyleSheet, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import AppNavigator from './navigation/AppNavigation';
 
 export default function App() {
-  const [userData, setData] = useState([]);
-
-  {/* we can go to this link to see debugging info for backend */}
-  useEffect(() => {
-    axios.get('http://localhost:8080/api/users')
-      .then(response => {
-        setData(response.data);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }, []); 
-
-  const renderItem = ({ item }) => (
-    <Text style={styles.item}> {item.name}: {item.email} </Text>
-  );
-
   return (
+    <NavigationContainer>
+      <AppNavigator />
+    </NavigationContainer>
+
+    /*
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <Text>Below is all the users: </Text>
-        {/* causes list to move above text out */}
         <FlatList
           contentContainerStyle={{flexGrow: 1, justifyContent: 'center'}}
           data={userData}
@@ -34,20 +23,6 @@ export default function App() {
         />
       <StatusBar style="auto" />
     </View>
+    */
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  item: {
-    paddingVertical: 10,
-    paddingHorizontal: 15,
-    backgroundColor: '#f0f0f0',
-    borderRadius: 5,
-  },
-});
