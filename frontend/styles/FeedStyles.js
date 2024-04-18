4b27b7dca613752f9276949185ae7fc22e400603
Fardin Haque <fardinhaque60@gmail.com>
2024-04-17 22:44:58 -0700

    styling for frontend

---
+++ frontend/styles/FeedStyles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+      },
+    header: {
+      fontSize: 24,
+      fontWeight: 'bold',
+    }
+  });
+
+export default styles;
