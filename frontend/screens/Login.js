fcf97662f5b65f00c194b257f91f998d71a2025b
Fardin Haque <fardinhaque60@gmail.com>
2024-04-18 11:15:49 -0700

    login UI complete

--- frontend/screens/Login.js
+++ frontend/screens/Login.js
@@ -1,30 +1,59 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, TouchableOpacity, Image, TextInput, Button } from 'react-native';
 import axios from 'axios';
 import styles from "../styles/LoginStyles";
+import { LinearGradient } from 'expo-linear-gradient';
 
 function Login({ navigation }) {
-  {/* we can go to this link to see debugging info for backend */}
-  useEffect(() => {
-    axios.get('http://localhost:8080/api/users')
+  const [loginForm, setForm] = useState({
+    'email': '',
+    'password': '',
+  })
+
+  const handleLogin = () => {
+    axios.post('http://localhost:8080/api/login', loginForm)
       .then(response => {
-        setData(response.data);
+        console.log(response.data);
+        navigation.navigate('Feed');
       })
       .catch(error => {
-        console.log(error);
+        console.log(error.response.data);
       });
-  }, []); 
+  } 
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Enter Username</Text>
-      <Text>Enter Password</Text>
-      <Button
-        title="Login"
-        onPress={() => navigation.navigate('Feed')}
-        style={styles.button}
-      />
-    </View>
+    <LinearGradient
+      colors={['#549da1', '#cc5bd9']}
+      style={styles.container}
+    > 
+      <View style={styles.overlay}>
+        <Image source={require('../assets/KnitsLogo.png')} style={styles.logo} />
+        <TextInput
+          style={styles.input}
+          keyboardType="email-address"
+          placeholder='Email'
+          autoCapitalize="none"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder='Password'
+          secureTextEntry
+        />
+        <TouchableOpacity onPress={handleLogin} style={styles.loginButton}>  
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+        <View style={styles.bottomContainer}>
+          <View style={styles.horizontalLine}></View> 
+          <Text style={styles.text}> Or </Text> 
+          <View style={styles.horizontalLine}></View> 
+        </View>
+        <Button
+          title='Create Account'
+          color='#0c1bf2'
+          onPress={() => navigation.navigate('Create-Account')}
+        />
+      </View>
+    </LinearGradient>
   );
 }
 
