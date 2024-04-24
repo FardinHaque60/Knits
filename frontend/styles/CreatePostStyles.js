2c61e7ee9469c6b13871183c2ca1bcceb4a799f0
Fardin Haque <fardinhaque60@gmail.com>
2024-04-24 16:23:25 -0700

    create post UI

---
+++ frontend/styles/CreatePostStyles.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from "react-native";
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    input: {
+        width: '100%',
+        height: 150,
+        borderWidth: 1,
+        borderColor: '#ccc',
+        borderRadius: 10,
+        paddingHorizontal: 10,
+        marginBottom: 20,
+    },
+})
+
+export default styles;
