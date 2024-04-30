cd026a438370584f5e8a191ca3b313a27643ab83
Fardin Haque <fardinhaque60@gmail.com>
2024-04-30 15:05:06 -0700

    removed unused imports

--- backend/src/main/java/com/example/backend/entity/User.java
+++ backend/src/main/java/com/example/backend/entity/User.java
@@ -1,15 +1,10 @@
 package com.example.backend.entity;
 
-import java.util.List;
-
-
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 
 @Entity // This tells Hibernate to make a table out of this class
 public class User {
